eb6a8f7f60bd043a9bc01d36b2a98abfce68c1f0
Daniel <danstad2012@gmail.com>
2020-05-05 14:22:15 -0700

    pre clear menagerie

--- src/utils/canvas.js
+++ src/utils/canvas.js
@@ -19,3 +19,35 @@ addEventListener('resize', function(){
 
   init()
 })
+
+const Object = (x,y,r, color) => {
+  this.x = x
+  this.y = y
+  this.r = r
+  this.color = color
+
+  this.update = () => {
+    this.draw()
+  }
+
+  this.draw = () => {
+    c.beginPath()
+    c.arc(this.x, this.y, this.r,0, Math.PI*2)
+    c.fillStyle = this.color
+    c.fill()
+    c.closePath()
+  }
+}
+let c1;
+function init() {
+  c1 = new Object(300,300,100, 'blue')
+}
+
+function animate() {
+  requestAnimationFrame(animate)
+  c.clearRect(0,0,canvas.width, canvas.height)
+  c.fillText("testing", mouse.x, mouse.y)
+}
+
+init()
+animate()
