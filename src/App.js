ec3ede197d5026fc0c42ce22d1ee9f79047d2ce1
Daniel Stadler <daniel.stadler.x@gmail.com>
2020-11-11 12:03:13 -0800

    more file restructuring

--- src/App.js
+++ src/App.js
@@ -2,28 +2,18 @@ import React from "react";
 import { useRoutes } from 'hookrouter';
 import { useRecoilState } from 'recoil';
 
-import Nav from './components/Nav.tsx'
-import Home from './Pages/Home'
-import About from './Pages/About'
-import Projects from './Pages/Projects'
-import Contact from './Pages/Contact'
+import Nav from './components/Nav'
 import Footer from './components/Footer'
 
 import { Body, Section } from './styles'
 import { hueState } from './utils/store'
+import routes from './components/Segments/Routes'
 
-const routes = {
-  '/': () => <Home />,
-  '/about': () => <About />,
-  '/projects': () => <Projects/>,
-  '/contact': () => <Contact/>,
-}
 
 const App = () => {
   let match = useRoutes(routes)
   const [hue, setHue] = useRecoilState(hueState)
   const colorShift = () => { setHue(hue % 360 + 3) }
-
 
   return (
     <Body hue={hue} onClick={() => colorShift()} >
--- src/styles/index.js
+++ src/styles/index.js
@@ -151,13 +151,4 @@ export const Question = styled.div`
   }
 `
 
-export const FooterContainer = styled.footer`
-  display: flex;
-  flex-direction: column;
-  justify-content: center;
-  background: black;
-  align-items: center;
-  height: 8vh;
-  color: white;
-  `
 
